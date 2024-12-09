f26e515c48c2b2c93d290fa7ae81ef7fd68c6086
Abhinandh S <ugabhi@proton.me>
2024-12-09 12:06:48 +0530

    added h tag

--- lua/just_snippets/typst/init.lua
+++ lua/just_snippets/typst/init.lua
@@ -2,7 +2,7 @@ local typst_snippets = {}
 
 -- Import snippets from different files
 typst_snippets.model = require("just_snippets.typst.model").snippets
--- typst_snippets.markup = require("just_snippets.typst.markup").snippets
+typst_snippets.markup = require("just_snippets.typst.markup").snippets
 -- typst_snippets.math = require("just_snippets.typst.math").snippets
 
 return typst_snippets
--- lua/just_snippets/typst/markup.lua
+++ lua/just_snippets/typst/markup.lua
@@ -1,1 +1,29 @@
-g
+local ls = require("luasnip")
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+
+-- Helper function to generate header snippets
+local function generate_header_snippet(level)
+  return s({
+      trig = "h" .. level, -- Trigger: h1, h2, h3, etc.
+      snippetType = "snippet",
+      desc = "Add header level " .. level,
+      wordTrig = true
+    },
+    {
+      t(string.rep("=", level) .. " "), -- Repeat "=" `level` times
+      i(1)  -- Insert node for the header text
+    }
+  )
+end
+
+-- Generate snippets for headers h1 to h7
+local markup_snippets = {}
+markup_snippets.snippets = {}
+
+for level = 1, 7 do
+  table.insert(markup_snippets.snippets, generate_header_snippet(level))
+end
+
+return markup_snippets
